ed4111a15f2eab2612515899cae74178263e507e
NeoBTK <neobtk@gmail.com>
2025-06-11 00:17:04 +0200

    Added classes from 27.1 and created tests for mappers

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -1,7 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
-import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
@@ -26,8 +26,8 @@ public class TrelloService {
     }
 
     // W materiałach jest użyty konstruktor w klasie Mail, ale w ostatnim zadaniu należało przerobić ją na Bildera dlatego tu go używam
-    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
-        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+    public CreatedTrelloCardDto createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(
                 Mail.builder()
                         .mailTo(adminConfig.getAdminMail())
